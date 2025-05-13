d7defac49addacc77fbfb60ca328893411a09ea3
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-05-13 14:08:42 +0900

    [Silver II] Title: DFS와 BFS, Time: 316 ms, Memory: 36612 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,34 +1,40 @@
+import sys
 from collections import deque
 
-def dfs(node,visited):
-    visited[node]=True
-    print(node,end=' ')
-    for neighbor in graph[node]:
-        if not visited[neighbor]:
-            dfs(neighbor,visited)
-
-def bfs(start):
+def dfs(node): #재귀 대신 스택으로 구현한 dfs
     visited=[False]*(N+1)
-    dq=deque([start])
-    visited[start]=True
-    while dq:
-        now=dq.popleft()
+    stack=deque()
+    stack.append(node)
+    while stack:
+        now=stack.pop()
+        if not visited[now]:
+            print(now,end=" ")
+            visited[now]=True
+            for neighbor in reversed(graph[now]):
+                if not visited[neighbor]:
+                    stack.append(neighbor)
+
+def bfs(node):
+    visited=[False]*(N+1)
+    queue=deque()
+    queue.append(node)
+    visited[node]=True
+    while queue:
+        now=queue.popleft()
         print(now,end=" ")
         for neighbor in graph[now]:
             if not visited[neighbor]:
                 visited[neighbor]=True
-                dq.append(neighbor)
-
+                queue.append(neighbor)
+    
 
 N,M,V=map(int,input().split())
-edges=[tuple(map(int,input().split())) for _ in range(M)]
-edges.sort(key=lambda x:(min(x),max(x)))
+input_links=[tuple(map(int,input().split())) for _ in range(M)]
+input_links.sort(key=lambda x:(min(x),max(x))) # 안해주면 연결그래프에 정렬이안됨 (작은 값 노드부터 탐색X)
 graph=[[] for _ in range(N+1)]
-for u,v in edges:
+for u,v in input_links:
     graph[u].append(v)
     graph[v].append(u)
-visited = [False] * (N + 1)
-dfs(V,visited)
+dfs(V)
 print("")
 bfs(V)
-print("")
